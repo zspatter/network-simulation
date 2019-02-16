fdad417d1197444d06e6397c23b65a56bf1e4f52
Zachary Patterson <zach.s.patterson@gmail.com>
2019-02-16 12:01:40 -0500

    This commit adds object definitions and refactors function calls.
    This was done to make it clear what Node and Network objects should consist of.
    
    The refactoring make the script names more consistent and simplified function calls in the Network class. The was done to improve readability.

--- Network.py
+++ Network.py
@@ -2,6 +2,10 @@ from Node import Node
 
 
 class Network:
+    # a Network instance consists of:
+    # graph_id (int)
+    # label (string)
+    # network_dict (node_id: Node)
     def __init__(self, graph_id, label, network_dict=None):
         self.graph_id = graph_id
         self.label = label
@@ -12,14 +16,15 @@ class Network:
         # ensures adjacency lists mirror each other (undirected weighted edges)
         nodes = network_dict.keys()
         for node in nodes:
-            adjacents = network_dict[node].adjacency_list.keys()
+            adjacents = network_dict[node].get_adjacents()
             for adjacent in adjacents:
-                if node in self.network_dict[adjacent].adjacency_list.keys():
+                if node in self.network_dict[adjacent].get_adjacents():
                     continue
                 else:
-                    self.network_dict[adjacent].adjacency_list[node] = \
-                        self.network_dict[node].adjacency_list[adjacent]
+                    self.network_dict[adjacent].adjacency_dict[node] = \
+                        self.network_dict[node].adjacency_dict[adjacent]
 
+    # determines if the network is connected (path between all nodes)
     def is_connected(self, nodes_encountered=None, start_node=None):
         if nodes_encountered is None:
             nodes_encountered = set()
@@ -30,7 +35,7 @@ class Network:
             start_node = nodes[0]
         nodes_encountered.add(start_node)
         if len(nodes_encountered) != len(nodes):
-            for node in network_dict[start_node].adjacency_list.keys():
+            for node in network_dict[start_node].get_adjacents():
                 if node not in nodes_encountered:
                     if self.is_connected(nodes_encountered, node):
                         return True
@@ -41,23 +46,23 @@ class Network:
     def nodes(self):
         return list(self.network_dict.keys())
 
-    def add_node(self, node_id, adjacency_list, label):
+    def add_node(self, node_id, label, adjacency_dict):
         # if node_id is unique, add it to the network
         if node_id not in self.network_dict:
             # creates new node with pass parameters
-            self.network_dict[node_id] = Node(node_id, adjacency_list, label)
+            self.network_dict[node_id] = Node(node_id, label, adjacency_dict)
             # adds edges to the new node from the nodes in the adjacency list
             # (this ensures the edge is represented in both directions)
-            for key in adjacency_list:
-                self.network_dict[key].adjacency_list[node_id] = adjacency_list[key]
+            for key in adjacency_dict:
+                self.network_dict[key].adjacency_dict[node_id] = adjacency_dict[key]
 
     def remove_node(self, node_id):
         # gathers list of adjacent node id's
-        adjacency_list = self.network_dict[node_id].adjacency_list.keys()
+        adjacency_dict = self.network_dict[node_id].get_adjacents()
 
         # removes edges store on adjacent objects
-        for key in adjacency_list:
-            del self.network_dict[key].adjacency_list[node_id]
+        for key in adjacency_dict:
+            del self.network_dict[key].adjacency_dict[node_id]
 
         # removes node object
         del self.network_dict[node_id]
@@ -65,20 +70,21 @@ class Network:
 
     # add edge to both adjacency lists
     def add_edge(self, node_id1, node_id2, weight):
-        if node_id2 not in self.network_dict[node_id1].adjacency_list.keys() \
-                and node_id1 not in self.network_dict[node_id2].adjacency_list.keys():
-            self.network_dict[node_id1].adjacency_list[node_id2] = weight
-            self.network_dict[node_id2].adjacency_list[node_id1] = weight
+        if node_id2 not in self.network_dict[node_id1].get_adjacents() \
+                and node_id1 not in self.network_dict[node_id2].get_adjacents():
+            self.network_dict[node_id1].adjacency_dict[node_id2] = weight
+            self.network_dict[node_id2].adjacency_dict[node_id1] = weight
 
     def remove_edge(self, node_id1, node_id2):
-        del self.network_dict[node_id1].adjacency_list[node_id2]
-        del self.network_dict[node_id2].adjacency_list[node_id1]
+        del self.network_dict[node_id1].adjacency_dict[node_id2]
+        del self.network_dict[node_id2].adjacency_dict[node_id1]
 
     def __str__(self):
         string = str(self.graph_id) + " " + self.label
         for key in self.network_dict.keys():
             string += f'{self.network_dict[key].__str__()}'
         return string
+
 
 """
 The lines below behave as the main method in Java
@@ -88,16 +94,16 @@ the basic functions of our graph/network
 """
 
 # creates 10 individual nodes (hard coded adjacency lists to match)
-node1 = Node(1, {2: 3, 3: 1, 5: 4, 9: 2}, "A")
-node2 = Node(2, {1: 3, 4: 2, 5: 3, 7: 9, 8: 10, 9: 4}, "B")
-node3 = Node(3, {1: 1, 4: 2, 6: 4, 7: 3}, "C")
-node4 = Node(4, {2: 2, 3: 2, 6: 8, 10: 4}, "D")
-node5 = Node(5, {1: 4, 2: 3, 8: 3, 9: 4}, "E")
-node6 = Node(6, {3: 4, 4: 8, 9: 8, 10: 2}, "F")
-node7 = Node(7, {2: 9, 3: 3, 8: 2, 10: 5}, "G")
-node8 = Node(8, {2: 10, 5: 3, 7: 2, 9: 4}, "H")
-node9 = Node(9, {1: 2, 2: 4, 5: 4, 6: 8, 8: 4}, "I")
-node10 = Node(10, {4: 4, 6: 2, 7: 5}, "J")
+node1 = Node(1, 'A', {2: 3, 3: 1, 5: 4, 9: 2})
+node2 = Node(2, 'B', {1: 3, 4: 2, 5: 3, 7: 9, 8: 10, 9: 4})
+node3 = Node(3, 'C', {1: 1, 4: 2, 6: 4, 7: 3})
+node4 = Node(4, 'D', {2: 2, 3: 2, 6: 8, 10: 4})
+node5 = Node(5, 'E', {1: 4, 2: 3, 8: 3, 9: 4})
+node6 = Node(6, 'F', {3: 4, 4: 8, 9: 8, 10: 2})
+node7 = Node(7, 'G', {2: 9, 3: 3, 8: 2, 10: 5})
+node8 = Node(8, 'H', {2: 10, 5: 3, 7: 2, 9: 4})
+node9 = Node(9, 'I', {1: 2, 2: 4, 5: 4, 6: 8, 8: 4})
+node10 = Node(10, 'J', {4: 4, 6: 2, 7: 5})
 
 # create a network consisting of the nodes above
 network = Network(1, 'Sample Network', {node1.node_id: node1,
@@ -126,17 +132,17 @@ network.remove_node(3)
 print(network.__str__() + '\n\n===============================\n')
 
 # demonstrates the add node function works
-network.add_node(11, {1: 3, 2: 4, 10: 8}, 'K')
+network.add_node(11, 'K', {1: 3, 2: 4, 10: 8})
 print(network.__str__() + '\n\n===============================\n')
 
 
 # tests the functionality in __init__ that ensures adjacency lists
 # of separate nodes mirror each other (undirected, weighted edges)
-init_tester_node1 = Node(1, {2: 3, 3: 5}, "A")
-init_tester_node2 = Node(2, {1: 2, 4: 3}, "B")
-init_tester_node3 = Node(3, {2: 6, 4: 9}, "C")
-init_tester_node4 = Node(4, {3: 8, 2: 4}, "D")
-init_tester_node5 = Node(5, {1: 1, 4: 2}, "E")
+init_tester_node1 = Node(1, "A", {2: 3, 3: 5})
+init_tester_node2 = Node(2, "B", {1: 2, 4: 3})
+init_tester_node3 = Node(3, "C", {2: 6, 4: 9})
+init_tester_node4 = Node(4, "D", {3: 8, 2: 4})
+init_tester_node5 = Node(5, "E", {1: 1, 4: 2})
 
 init_tester = Network(1, "__init__ Test Network", {init_tester_node1.node_id: init_tester_node1,
                                       init_tester_node2.node_id: init_tester_node2,
--- Node.py
+++ Node.py
@@ -1,28 +1,31 @@
 class Node:
-    def __init__(self, node_id, adjacency_list, label):
+    # a Node instance consists of:
+    # node_id (int)
+    # label (string)
+    # adjacency_dict (node_id: weight)
+    def __init__(self, node_id, label='Default Node', adjacency_dict=None):
         self.node_id = node_id
-        self.adjacency_list = adjacency_list
         self.label = label
+        if adjacency_dict is None:
+            adjacency_dict = {}
+        self.adjacency_dict = adjacency_dict
 
-
-    # if 'key' in dict:
+    # return bool indicating whether this instance shares an edge with passed node_id
     def is_neighbor(self, node_id):
-        if node_id in self.adjacency_list:
+        if node_id in self.adjacency_dict:
             return True
         else:
             return False
 
-
-    def edges(self):
-        return list(self.adjacency_list.keys())
-
+    def get_adjacents(self):
+        return list(self.adjacency_dict.keys())
 
     def __del__(self):
         print(f'Destructor called, Node #{self.node_id}: {self.label} removed.')
 
     def __str__(self):
-        string = '\n\nLabel: ' + self.label +  '\t(node id: ' + str(self.node_id) + ')\nNeighbors:'
-        for key in self.adjacency_list:
-            string += '\n\tNode #' + str(key) + ': weight - ' + str(self.adjacency_list[key])
+        string = '\n\nLabel: ' + self.label + '\t(node id: ' + str(self.node_id) + ')\nNeighbors:'
+        for key in self.adjacency_dict:
+            string += '\n\tNode #' + str(key) + ': weight - ' + str(self.adjacency_dict[key])
         return string
 
