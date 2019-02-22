27c9642214ed56a0ae683aa7546121d70fa5d477
Zachary Patterson <33292185+zspatter@users.noreply.github.com>
2019-02-22 07:50:35 -0500

    This commit migrates the tests to a new script.
    This was done to separate our class and execution. Everything that was present at the bottom of the Network class has been moved to network_test.

---
+++ network_test.py
@@ -0,0 +1,236 @@
+from Node import Node
+from Network import Network
+import time
+
+"""
+The lines below behave as the main method in Java
+
+Everything below is just to briefly demonstrate/test
+the basic functions of our graph/network
+"""
+
+# creates 10 individual nodes (hard coded adjacency lists to match)
+node_1 = Node(1, 'A',
+              {2: {'weight': 3, 'status': True},
+               3: {'weight': 1, 'status': True},
+               5: {'weight': 4, 'status': True},
+               9: {'weight': 2, 'status': True}})
+node_2 = Node(2, 'B',
+              {1: {'weight': 3, 'status': True},
+               4: {'weight': 2, 'status': True},
+               5: {'weight': 3, 'status': True},
+               7: {'weight': 9, 'status': True},
+               8: {'weight': 10, 'status': True},
+               9: {'weight': 4, 'status': True}})
+node_3 = Node(3, 'C',
+              {1: {'weight': 1, 'status': True},
+               4: {'weight': 2, 'status': True},
+               6: {'weight': 4, 'status': True},
+               7: {'weight': 3, 'status': True}})
+node_4 = Node(4, 'D',
+              {2: {'weight': 2, 'status': True},
+               3: {'weight': 2, 'status': True},
+               6: {'weight': 8, 'status': True},
+               10: {'weight': 4, 'status': True}})
+node_5 = Node(5, 'E',
+              {1: {'weight': 4, 'status': True},
+               2: {'weight': 3, 'status': True},
+               8: {'weight': 3, 'status': True},
+               9: {'weight': 4, 'status': True}})
+node_6 = Node(6, 'F',
+              {3: {'weight': 4, 'status': True},
+               4: {'weight': 8, 'status': True},
+               9: {'weight': 8, 'status': True},
+               10: {'weight': 2, 'status': True}})
+node_7 = Node(7, 'G',
+              {2: {'weight': 9, 'status': True},
+               3: {'weight': 3, 'status': True},
+               8: {'weight': 2, 'status': True},
+               10: {'weight': 5, 'status': True}})
+node_8 = Node(8, 'H',
+              {2: {'weight': 10, 'status': True},
+               5: {'weight': 3, 'status': True},
+               7: {'weight': 2, 'status': True},
+               9: {'weight': 4, 'status': True}})
+node_9 = Node(9, 'I',
+              {1: {'weight': 2, 'status': True},
+               2: {'weight': 4, 'status': True},
+               5: {'weight': 4, 'status': True},
+               6: {'weight': 8, 'status': True},
+               8: {'weight': 4, 'status': True}})
+node_10 = Node(10, 'J',
+               {4: {'weight': 4, 'status': True},
+                6: {'weight': 2, 'status': True},
+                7: {'weight': 5, 'status': True}})
+
+# create a network consisting of the nodes above
+network_1 = Network(1, 'Sample Network',
+                    {node_1.node_id: node_1,
+                     node_2.node_id: node_2,
+                     node_3.node_id: node_3,
+                     node_4.node_id: node_4,
+                     node_5.node_id: node_5,
+                     node_6.node_id: node_6,
+                     node_7.node_id: node_7,
+                     node_8.node_id: node_8,
+                     node_9.node_id: node_9,
+                     node_10.node_id: node_10})
+
+
+# demonstrates __str__() function works
+print(network_1.__str__() + '\n\n===============================\n')
+
+# demonstrates remove_edge function works
+print('\t---REMOVE EDGE---\n')
+network_1.remove_edge(1, 2)
+print(network_1.__str__() + '\n\n===============================\n')
+
+# demonstrates remove_node function works
+print('\t---REMOVE NODE---\n')
+network_1.remove_node(3)
+print(network_1.__str__() + '\n\n===============================\n')
+
+# demonstrates add edge function works
+print('\t---ADD EDGE---\n')
+network_1.add_edge(1, 2, 15)
+print(network_1.__str__() + '\n\n===============================\n')
+
+# demonstrates the add node function works
+print('\t---ADD NODE---\n')
+network_1.add_node(11, 'K',
+                   {1: {'weight': 3, 'status': True},
+                    2: {'weight': 4, 'status': True},
+                    10: {'weight': 8, 'status': True}})
+print(network_1.__str__() + '\n\n===============================\n')
+
+
+# demonstrates is_connected() function works
+if network_1.is_connected():
+    print("The network is connected!\n")
+else:
+    print("The network is not connected!\n")
+start = time.time_ns()
+print('Recursive DFS: ' + str(network_1.is_connected()))
+end = time.time_ns()
+print('\telapsed time: ' + str(end - start))
+
+start = time.time_ns()
+print('\nIterative DFS: ' + str(network_1.DFS()))
+end = time.time_ns()
+print('\telapsed time: ' + str(end - start))
+
+start = time.time_ns()
+print('\nIterative BFS: ' + str(network_1.BFS()))
+end = time.time_ns()
+print('\telapsed time: ' + str(end - start) + '\n\n===============================\n')
+
+
+# creates disconnected network to further test is_connected()
+node_a = Node(1, 'A',
+              {2: {'weight': 1, 'status': True},
+               3: {'weight': 2, 'status': True}})
+node_b = Node(2, 'B',
+              {3: {'weight': 1, 'status': True}})
+node_c = Node(3, 'C',
+              {1: {'weight': 2, 'status': True}})
+node_d = Node(4, 'D')
+disconnected_network = Network(1, 'Disconnected Network',
+                               {1: node_a,
+                                2: node_b,
+                                3: node_c,
+                                4: node_d})
+
+# prints Disconnected Network and checks if graph is connected
+print('\t---DISCONNECTED GRAPH---\n')
+print(disconnected_network.__str__() + '\n\n===============================\n')
+
+
+start = time.time_ns()
+print('Recursive DFS: ' + str(disconnected_network.is_connected()))
+end = time.time_ns()
+print('\telapsed time: ' + str(end - start))
+
+start = time.time_ns()
+print('\nIterative DFS: ' + str(disconnected_network.DFS()))
+end = time.time_ns()
+print('\telapsed time: ' + str(end - start))
+
+start = time.time_ns()
+print('\nIterative BFS: ' + str(disconnected_network.BFS()))
+end = time.time_ns()
+print('\telapsed time: ' + str(end - start) + '\n\n===============================\n')
+
+
+# tests the functionality in __init__ that ensures adjacency lists
+# of separate nodes mirror each other (undirected, weighted edges)
+init_tester_node1 = Node(1, "A",
+                         {2: {'weight': 3, 'status': True},
+                          3: {'weight': 5, 'status': True}})
+init_tester_node2 = Node(2, "B",
+                         {1: {'weight': 2, 'status': True},
+                          4: {'weight': 3, 'status': True}})
+init_tester_node3 = Node(3, "C",
+                         {2: {'weight': 6, 'status': True},
+                          4: {'weight': 9, 'status': True}})
+init_tester_node4 = Node(4, "D",
+                         {3: {'weight': 8, 'status': True},
+                          2: {'weight': 4, 'status': True}})
+init_tester_node5 = Node(5, "E",
+                         {1: {'weight': 1, 'status': True},
+                          4: {'weight': 2, 'status': True}})
+
+init_tester = Network(1, "__init__ Test Network",
+                      {init_tester_node1.node_id: init_tester_node1,
+                       init_tester_node2.node_id: init_tester_node2,
+                       init_tester_node3.node_id: init_tester_node3,
+                       init_tester_node4.node_id: init_tester_node4,
+                       init_tester_node5.node_id: init_tester_node5})
+
+
+print('\t---ADJACENCY LISTS MIRROR TEST---\n')
+print(init_tester.__str__() + '\n\n===============================\n')
+
+print('\t---MARK EDGE BETWEEN NODE #1 AND #5 INACTIVE---\n')
+init_tester.mark_edge_inactive(1, 5)
+print(init_tester.__str__() + '\n\n===============================\n')
+
+print('\t---MARK NODE #1 INACTIVE---\n')
+init_tester.mark_node_inactive(1)
+print(init_tester.__str__() + '\n\n===============================\n')
+
+
+start = time.time_ns()
+print('Recursive DFS: ' + str(init_tester.is_connected()))
+end = time.time_ns()
+print('\telapsed time: ' + str(end - start))
+
+start = time.time_ns()
+print('\nIterative DFS: ' + str(init_tester.DFS()))
+end = time.time_ns()
+print('\telapsed time: ' + str(end - start))
+
+start = time.time_ns()
+print('\nIterative BFS: ' + str(init_tester.BFS()))
+end = time.time_ns()
+print('\telapsed time: ' + str(end - start) + '\n\n===============================\n')
+
+print('\t---MARK NODE #1 ACTIVE---\n')
+init_tester.mark_node_active(1)
+print(init_tester.__str__() + '\n\n===============================\n')
+
+
+# tests the generate_network and generate_adjacency_list functions
+generated_network = Network.generate_network(5000)
+print('\t---GENERATED RANDOM NETWORK---\n')
+print(generated_network.__str__() + '\n\n===============================\n')
+
+# times iterative DFS and iterative BFS for comparison
+start = time.time_ns()
+print('\nIterative DFS: ' + str(generated_network.DFS()))
+end = time.time_ns()
+print('\telapsed time: ' + str(end - start))
+
+start = time.time_ns()
+print('\nIterative BFS: ' + str(generated_network.BFS()))
+end = time.time_ns()
+print('\telapsed time: ' + str(end - start))
