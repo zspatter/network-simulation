f7db2d6b7778a1f4aa1aef498de06743f191416e
Zachary Patterson <zach.s.patterson@gmail.com>
2019-03-18 20:43:51 -0400

    Adds patient initializer

--- Network_Simulator/Patient.py
+++ Network_Simulator/Patient.py
@@ -5,3 +5,11 @@ class Patient:
     Each patient is defined with a name, a unique ID, illness, organ needed,
     waiting time (priority?), and location ('home' hospital).
     """
+
+    def __init__(self, patient_name, id, illness, organ_needed, wait_time, location):
+        self.patient_name = patient_name
+        self.id = id
+        self.illness = illness
+        self.organ_needed = organ_needed
+        self.wait_time = wait_time
+        self.location = location
