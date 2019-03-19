2412d4bf215bd9c1cffd808418948fb14088e7d3
Zachary Patterson <zach.s.patterson@gmail.com>
2019-03-18 20:38:35 -0400

    Adds Organ class

---
+++ Network_Simulator/Organ.py
@@ -0,0 +1,7 @@
+class Organ:
+    """
+    A class representing a given organ which is available for transplant.
+
+    Each organ has a name, a unique ID, a maximum out of body duration,
+    type matching, and a location.
+    """
