24f1dba0f246a6a1c8ccefd14c7083ddddc71b62
Zachary Patterson <zach.s.patterson@gmail.com>
2019-03-18 20:44:00 -0400

    Adds organ initializer

--- Network_Simulator/Organ.py
+++ Network_Simulator/Organ.py
@@ -2,6 +2,12 @@ class Organ:
     """
     A class representing a given organ which is available for transplant.
 
-    Each organ has a name, a unique ID, a maximum out of body duration,
+    Each organ has a name, a unique ID, lifetime (a maximum out of body duration),
     type matching, and a location.
     """
+    def __init__(self, organ_name, id, lifetime, organ_type, location):
+        self.organ_name = organ_name
+        self.id = id
+        self.lifetime = lifetime
+        self.organ_type = organ_type
+        self.location = location
