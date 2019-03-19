d3ec552fd71aa20dda7674688bac3a250e1eb7b6
Zachary Patterson <zach.s.patterson@gmail.com>
2019-03-18 20:38:28 -0400

    Adds Patient class

---
+++ Network_Simulator/Patient.py
@@ -0,0 +1,7 @@
+class Patient:
+    """
+    A class representing a patient in need of an organ transplant.
+
+    Each patient is defined with a name, a unique ID, illness, organ needed,
+    waiting time (priority?), and location ('home' hospital).
+    """
