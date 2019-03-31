0a79f2c3bac87f271863e1303588572b187631d8
Zachary Patterson <zach.s.patterson@gmail.com>
2019-03-30 20:33:35 -0400

    Completes unit tests for all functions in Dijkstra class

--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -1,16 +1,15 @@
-import network_simulator.Node as n
 import network_simulator.Network as net
 import network_simulator.Dijkstra as d
 
 
 def test_is_connected():
     test_net = net.Network()
-    test_net.add_node(n.Node(1))
+    test_net.add_node(net.Node(1))
     test_dijkstra = d.Dijkstra(graph=test_net, source=1)
 
     assert test_dijkstra.is_connected(test_net)
 
-    test_net.add_node(n.Node(2))
+    test_net.add_node(net.Node(2))
     assert not test_dijkstra.is_connected(test_net)
 
     test_net.add_edge(node_id1=1, node_id2=2, weight=5)
@@ -19,12 +18,12 @@ def test_is_connected():
 
 def test_DFS():
     test_net = net.Network()
-    test_net.add_node(n.Node(1))
+    test_net.add_node(net.Node(1))
     test_dijkstra = d.Dijkstra(graph=test_net, source=1)
 
     assert test_dijkstra.DFS(test_net)
 
-    test_net.add_node(n.Node(2))
+    test_net.add_node(net.Node(2))
     assert not test_dijkstra.DFS(test_net)
 
     test_net.add_edge(node_id1=1, node_id2=2, weight=5)
@@ -33,13 +32,114 @@ def test_DFS():
 
 def test_BFS():
     test_net = net.Network()
-    test_net.add_node(n.Node(1))
-    test_dijkstra = d.Dijkstra(graph=test_net, source=1)
+    test_net.add_node(net.Node(1))
+    test_d = d.Dijkstra(graph=test_net, source=1)
 
-    assert test_dijkstra.BFS(test_net)
+    assert test_d.BFS(test_net)
 
-    test_net.add_node(n.Node(2))
-    assert not test_dijkstra.BFS(test_net)
+    test_net.add_node(net.Node(2))
+    assert not test_d.BFS(test_net)
 
     test_net.add_edge(node_id1=1, node_id2=2, weight=5)
-    assert test_dijkstra.BFS(test_net)
+    assert test_d.BFS(test_net)
+
+
+def test_dijkstra():
+    test_node1 = net.Node(node_id=1, adjacency_dict={2: {'weight': 5, 'status': True}})
+    test_node2 = net.Node(node_id=2, adjacency_dict={3: {'weight': 5, 'status': True}})
+    test_node3 = net.Node(node_id=3)
+    test_node4 = net.Node(node_id=4)
+    test_net = net.Network({test_node1.node_id: test_node1,
+                            test_node2.node_id: test_node2,
+                            test_node3.node_id: test_node3,
+                            test_node4.node_id: test_node4})
+
+    weight, previous = d.Dijkstra.dijkstra(graph=test_net, source=1)
+    assert weight[1] is 0
+    assert weight[2] is 5
+    assert weight[3] is 10
+    assert weight[4] == float('inf')
+    assert previous[1] is None
+    assert previous[2] is 1
+    assert previous[3] is 2
+    assert previous[4] is None
+
+
+def test_minimum_unvisited_distance():
+    unvisited = [1, 2, 3, 4, 5]
+    weight = dict.fromkeys(unvisited, float('inf'))
+
+    assert d.Dijkstra.minimum_unvisited_distance(unvisited, weight) is 1
+    weight[2] = 0
+    assert d.Dijkstra.minimum_unvisited_distance(unvisited, weight) is 2
+    weight[3] = 3
+    weight[5] = 5
+    assert d.Dijkstra.minimum_unvisited_distance(unvisited, weight) is 2
+    unvisited.remove(2)
+    assert d.Dijkstra.minimum_unvisited_distance(unvisited, weight) is 3
+    unvisited.remove(3)
+    assert d.Dijkstra.minimum_unvisited_distance(unvisited, weight) is 5
+    unvisited.remove(5)
+    assert d.Dijkstra.minimum_unvisited_distance(unvisited, weight) is 1
+
+
+def test_shortest_path():
+    test_node1 = net.Node(node_id=1, adjacency_dict={2: {'weight': 10, 'status': True}})
+    test_node2 = net.Node(node_id=2, adjacency_dict={3: {'weight': 5, 'status': True}})
+    test_node3 = net.Node(node_id=3, adjacency_dict={4: {'weight': 3, 'status': True}})
+    test_node4 = net.Node(node_id=4, adjacency_dict={1: {'weight': 6, 'status': True}})
+    test_node5 = net.Node(node_id=5, adjacency_dict={1: {'weight': 1, 'status': True},
+                                                   3: {'weight': 2, 'status': True}})
+    test_net = net.Network({test_node1.node_id: test_node1,
+                            test_node2.node_id: test_node2,
+                            test_node3.node_id: test_node3,
+                            test_node4.node_id: test_node4,
+                            test_node5.node_id: test_node5})
+
+    dijkstra = d.Dijkstra(graph=test_net, source=test_node1.node_id)
+    path, weight = dijkstra.shortest_path(destination=test_node3.node_id)
+    assert path == [1, 5, 3]
+    assert weight is 3
+
+    test_net.remove_node(test_node5.node_id)
+    dijkstra = d.Dijkstra(graph=test_net, source=test_node1.node_id)
+    path, weight = dijkstra.shortest_path(destination=test_node3.node_id)
+    assert path == [1, 4, 3]
+    assert weight is 9
+
+    test_net.remove_node(test_node4.node_id)
+    dijkstra = d.Dijkstra(graph=test_net, source=test_node1.node_id)
+    path, weight = dijkstra.shortest_path(destination=test_node3.node_id)
+    assert path == [1, 2, 3]
+    assert weight is 15
+
+    test_net.remove_node(test_node2.node_id)
+    dijkstra = d.Dijkstra(graph=test_net, source=test_node1.node_id)
+    path, weight = dijkstra.shortest_path(destination=test_node3.node_id)
+    assert path is None
+    assert weight == float('inf')
+
+
+def test_all_shortest_paths():
+    test_node1 = net.Node(node_id=1, adjacency_dict={2: {'weight': 10, 'status': True}})
+    test_node2 = net.Node(node_id=2, adjacency_dict={3: {'weight': 5, 'status': True},
+                                                   4: {'weight': 10, 'status': True},
+                                                   5: {'weight': 15, 'status': True}})
+    test_node3 = net.Node(node_id=3)
+    test_node4 = net.Node(node_id=4)
+    test_node5 = net.Node(node_id=5)
+    test_net = net.Network({test_node1.node_id: test_node1,
+                            test_node2.node_id: test_node2,
+                            test_node3.node_id: test_node3,
+                            test_node4.node_id: test_node4,
+                            test_node5.node_id: test_node5})
+
+    dijkstra = d.Dijkstra(graph=test_net, source=test_node1.node_id)
+    shortest_paths = dijkstra.all_shortest_paths()
+    assert shortest_paths[1] == ([1], 0)
+    assert shortest_paths[2] == ([1, 2], 10)
+    assert shortest_paths[3] == ([1, 2, 3], 15)
+    assert shortest_paths[4] == ([1, 2, 4], 20)
+    assert shortest_paths[5] == ([1, 2, 5], 25)
+
+
