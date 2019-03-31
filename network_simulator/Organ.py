877ae818d84b8a2cd11d3e95c1398f584c6d97a8
Zachary Patterson <zach.s.patterson@gmail.com>
2019-03-31 13:31:30 -0400

    Adds function to move organs (alters organ attributes)

--- network_simulator/Organ.py
+++ network_simulator/Organ.py
@@ -5,9 +5,17 @@ class Organ:
     Each organ has a name, a unique ID, lifetime (a maximum out of body duration),
     type matching, and a location.
     """
-    def __init__(self, organ_name, id, lifetime, organ_type, location):
-        self.organ_name = organ_name
-        self.id = id
-        self.lifetime = lifetime
+    def __init__(self, organ_id, organ_category, organ_type, viability, location):
+        self.organ_id = organ_id
+        self.organ_category = organ_category
         self.organ_type = organ_type
+        self.viability = viability
         self.location = location
+
+    def move_organ(self, new_location, cost):
+        if self.viability < cost:
+            print('ERROR: organ no longer viable!')
+            return
+
+        self.location = new_location
+        self.viability -= cost
