150a884e5c9344707eec71d202598f24fdacd974
Zachary Patterson <zach.s.patterson@gmail.com>
2019-03-31 15:17:23 -0400

    Completes a very brief sanity check on the simulation

--- network_simulator/simulation.py
+++ network_simulator/simulation.py
@@ -1,6 +1,7 @@
 import network_simulator.Network as net
 import network_simulator.Organ as O
 import network_simulator.Patient as P
+import network_simulator.Dijkstra as D
 
 
 hospital_a = net.Node(1, 'A',
@@ -68,6 +69,7 @@ hospital_network = net.Network({hospital_a.node_id: hospital_a,
                                 hospital_j.node_id: hospital_j},
                                'Hospital Network')
 
+# creates a handful of patients who need organ (across network)
 patient_a = P.Patient(patient_id=1, patient_name='patient a', illness='diabetes',
                       organ_needed=O.Organ.PANCREAS, wait_time=1, location=hospital_j.node_id)
 patient_b = P.Patient(patient_id=2, patient_name='patient b', illness='heart trouble',
@@ -79,4 +81,50 @@ patient_d = P.Patient(patient_id=4, patient_name='patient d', illness='lung canc
 patient_e = P.Patient(patient_id=5, patient_name='patient e', illness='diabetes',
                       organ_needed=O.Organ.PANCREAS, wait_time=100, location=hospital_b.node_id)
 
+# harvests a handful of organs (single donor, same source location)
+harvest_organ_1 = O.Organ(organ_category=O.Organ.PANCREAS, organ_type='NA',
+                          viability=50, location=hospital_a.node_id)
+harvest_organ_2 = O.Organ(organ_category=O.Organ.HEART, organ_type='NA',
+                          viability=50, location=hospital_a.node_id)
+harvest_organ_3 = O.Organ(organ_category=O.Organ.LIVER, organ_type='NA',
+                          viability=50, location=hospital_a.node_id)
+harvest_organ_4 = O.Organ(organ_category=O.Organ.LUNG, organ_type='NA',
+                          viability=50, location=hospital_a.node_id)
 
+dijkstra = D.Dijkstra(hospital_network, harvest_organ_1.origin_location)
+
+path, weight = dijkstra.shortest_path(patient_a.location)
+harvest_organ_1.move_organ(patient_a.location, weight)
+print(f'Organ: {O.Organ.organ_category_name(harvest_organ_1.organ_category)}'
+      f' has been transported from hospital '
+      f'{harvest_organ_1.origin_location} to hospital {patient_a.location}.'
+      f'\n\tPath taken: {path}'
+      f'\n\tThis came with a cost of: {weight}'
+      f'\n\tRemaining organ viability is: {harvest_organ_1.viability}\n')
+
+path, weight = dijkstra.shortest_path(patient_b.location)
+harvest_organ_2.move_organ(patient_b.location, weight)
+print(f'Organ: {O.Organ.organ_category_name(harvest_organ_2.organ_category)}'
+      f' has been transported from hospital '
+      f'{harvest_organ_2.origin_location} to hospital {patient_b.location}.'
+      f'\n\tPath taken: {path}'
+      f'\n\tThis came with a cost of: {weight}'
+      f'\n\tRemaining organ viability is: {harvest_organ_2.viability}\n')
+
+path, weight = dijkstra.shortest_path(patient_c.location)
+harvest_organ_3.move_organ(patient_c.location, weight)
+print(f'Organ: {O.Organ.organ_category_name(harvest_organ_3.organ_category)}'
+      f' has been transported from hospital '
+      f'{harvest_organ_3.origin_location} to hospital {patient_c.location}.'
+      f'\n\tPath taken: {path}'
+      f'\n\tThis came with a cost of: {weight}'
+      f'\n\tRemaining organ viability is: {harvest_organ_3.viability}\n')
+
+path, weight = dijkstra.shortest_path(patient_d.location)
+harvest_organ_4.move_organ(patient_d.location, weight)
+print(f'Organ: {O.Organ.organ_category_name(harvest_organ_4.organ_category)}'
+      f' has been transported from hospital '
+      f'{harvest_organ_4.origin_location} to hospital {patient_d.location}.'
+      f'\n\tPath taken: {path}'
+      f'\n\tThis came with a cost of: {weight}'
+      f'\n\tRemaining organ viability is: {harvest_organ_4.viability}\n')
