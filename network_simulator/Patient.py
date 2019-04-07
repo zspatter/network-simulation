999f52c4b3a95a0cbecefdcde14f68854a015245
Zachary Patterson <zach.s.patterson@gmail.com>
2019-04-07 16:50:05 -0400

    Adds constants for organ types

--- network_simulator/Patient.py
+++ network_simulator/Patient.py
@@ -5,14 +5,43 @@ class Patient:
     Each patient is defined with a name, a unique ID, illness, organ needed,
     waiting time (priority?), and location ('home' hospital).
     """
-
+    # heart, kidneys, liver, lungs, pancreas, intestine, and thymus.
+    HEART = 1
+    KIDNEY = 2
+    LIVER = 3
+    LUNG = 4
+    PANCREAS = 5
+    INTESTINE = 6
+    THYMUS = 7
     patient_count = 0
 
-    def __init__(self, patient_name, illness, organ_needed, wait_time, location):
+    def __init__(self, patient_name, illness, organ_needed, priority, location):
         Patient.patient_count = Patient.patient_count + 1
         self.patient_id = Patient.patient_count
         self.patient_name = patient_name
         self.illness = illness
         self.organ_needed = organ_needed
-        self.wait_time = wait_time
+        self.priority = priority
         self.location = location
+
+    @staticmethod
+    def organ_category_name(n):
+        """
+        Returns the string associated with an organ category int
+        This is designed to improve readability with console output
+
+        :param int n: a number between 1-7 (inclusive) as defined with organ constants
+        :return: string representing organ (or None if no match found)
+        """
+        if 0 < n < 8:
+            organs = {Patient.HEART: 'Heart',
+                      Patient.KIDNEY: 'Kidney',
+                      Patient.LIVER: 'Liver',
+                      Patient.LUNG: 'Lung',
+                      Patient.PANCREAS: 'Pancreas',
+                      Patient.INTESTINE: 'Intestines',
+                      Patient.THYMUS: 'Thymus'}
+
+            return organs[n]
+
+        return None
