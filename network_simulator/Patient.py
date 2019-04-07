1011dbcdbf3ad14989d3725c6622ff831f30fcef
Zachary Patterson <zach.s.patterson@gmail.com>
2019-04-07 17:10:44 -0400

    Adds blood type compatibility function

--- network_simulator/Patient.py
+++ network_simulator/Patient.py
@@ -6,23 +6,48 @@ class Patient:
     waiting time (priority?), and location ('home' hospital).
     """
     # heart, kidneys, liver, lungs, pancreas, intestine, and thymus.
-    HEART = 1
-    KIDNEY = 2
-    LIVER = 3
-    LUNG = 4
-    PANCREAS = 5
-    INTESTINE = 6
-    THYMUS = 7
+    HEART = 0
+    KIDNEY = 1
+    LIVER = 2
+    LUNG = 3
+    PANCREAS = 4
+    INTESTINE = 5
+    THYMUS = 6
+    O_NEG = 0
+    O_POS = 1
+    A_NEG = 2
+    A_POS = 3
+    B_NEG = 4
+    B_POS = 5
+    AB_NEG = 6
+    AB_POS = 7
     patient_count = 0
 
-    def __init__(self, patient_name, illness, organ_needed, priority, location):
+    def __init__(self, patient_name, illness, organ_needed, blood_type, priority, location):
         Patient.patient_count = Patient.patient_count + 1
         self.patient_id = Patient.patient_count
         self.patient_name = patient_name
         self.illness = illness
         self.organ_needed = organ_needed
+        self.blood_type = blood_type
         self.priority = priority
         self.location = location
+
+    def blood_type_compatibility(self, blood_type):
+        if self.blood_type is blood_type:
+            return True
+        if self.blood_type % 2 is 1 and self.blood_type - blood_type is 1:
+            return True
+        if self.blood_type is Patient.AB_POS:
+            return True
+        if self.blood_type is Patient.AB_NEG and blood_type % 2 is 0:
+            return True
+        if blood_type is Patient.O_NEG:
+            return True
+        if blood_type is Patient.O_POS and self.blood_type % 2 is 1:
+            return True
+        return False
+
 
     @staticmethod
     def organ_category_name(n):
