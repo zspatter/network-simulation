d22ca6f6902ae1b5083880e50e3dee56a1efa8e9
Zachary Patterson <zach.s.patterson@gmail.com>
2019-04-08 18:34:10 -0400

    Adds function to generate organs to harvest from

--- network_simulator/OrganList.py
+++ network_simulator/OrganList.py
@@ -1,3 +1,6 @@
+import random
+import network_simulator.Organ as O
+
 class OrganList:
     """
     A class that represents all of the available organs waiting to be
@@ -25,3 +28,16 @@ class OrganList:
         :param Organ organ: object to be removed from the organ list
         """
         self.organ_list.remove(organ)
+
+    def generate_organs(self, graph, harvest_num):
+        node_count = len(graph.network_dict)
+
+        # number of patients to harvest from
+        for _ in range(harvest_num):
+            # number of possible organs to harvest
+            for x in range(6):
+                # determines if organ is suitable for harvest
+                if random.choice([True, False]):
+                    temp = O.Organ(organ_type=x, blood_type=random.randint(0, 7),
+                                   location=random.randrange(node_count),
+                                   organ_list=self)
