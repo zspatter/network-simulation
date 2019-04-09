defade00f4c7b27ac475d8092ace711c39897fa0
Zachary Patterson <localhost@zspatter.dev>
2019-04-09 10:57:54 -0400

    Extracts generate_patients() to dedicated class

---
+++ network_simulator/GeneratePatients.py
@@ -0,0 +1,27 @@
+import random
+import network_simulator.Patient as P
+
+
+class GeneratePatients:
+
+    @classmethod
+    def generate_patients(cls, graph, n, wait_list):
+        """
+        Generates n patients to add to wait list with random combinations of
+        organ needed, blood type, priority, and location
+
+        :param Network graph: network for patients to be allocated to
+        :param int n: number of patients to generate
+        :param WaitList wait_list: 
+        """
+        # list of currently active nodes
+        nodes = graph.nodes()
+
+        for x in range(n):
+            temp = P.Patient(patient_name="generated patient #" + str(x + 1),
+                             illness="N/A",
+                             organ_needed=random.randrange(7),
+                             blood_type=random.randrange(8),
+                             priority=random.randrange(251),
+                             location=random.choice(nodes),
+                             wait_list=wait_list)
--- network_simulator/WaitList.py
+++ network_simulator/WaitList.py
@@ -2,6 +2,7 @@ import heapq
 import random
 import network_simulator.Patient as P
 import network_simulator.MatchIdentifier as MI
+import network_simulator.GeneratePatients as GP
 
 
 class WaitList:
@@ -61,20 +62,10 @@ class WaitList:
 
     def generate_patients(self, graph, n):
         """
-        Generates n patients to add to wait list with random combinations of
-        organ needed, blood type, priority, and location
+        Wrapper that calls the GeneratePatients.generate_patients function
+        This is used to ensure the generated patients are added to the same wait list
 
         :param Network graph: network for patients to be allocated to
         :param int n: number of patients to generate
         """
-        # list of currently active nodes
-        nodes = graph.nodes()
-
-        for x in range(n):
-            temp = P.Patient(patient_name="generated patient #" + str(x + 1),
-                             illness="N/A",
-                             organ_needed=random.randrange(7),
-                             blood_type=random.randrange(8),
-                             priority=random.randrange(251),
-                             location=random.choice(nodes),
-                             wait_list=self)
+        GP.GeneratePatients.generate_patients(graph, n, self)
