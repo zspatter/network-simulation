e902b890bf6258744621b93cf43aed6e6338e141
Zachary Patterson <localhost@zspatter.dev>
2019-04-09 08:52:15 -0400

    restricts generate_organs to currently active nodes

--- network_simulator/OrganList.py
+++ network_simulator/OrganList.py
@@ -1,6 +1,7 @@
 import random
 import network_simulator.Organ as O
 
+
 class OrganList:
     """
     A class that represents all of the available organs waiting to be
@@ -29,13 +30,14 @@ class OrganList:
         """
         self.organ_list.remove(organ)
 
-    def generate_organs(self, graph, harvest_num):
-        node_count = len(graph.network_dict)
+    def generate_organs(self, graph, n):
+        # list of currently active nodes
+        nodes = graph.nodes()
 
         # number of patients to harvest from
-        for _ in range(harvest_num):
+        for _ in range(n):
             # number of possible organs to harvest
-            location_id = random.randrange(node_count)
+            location_id = random.choice(nodes)
             for x in range(6):
                 # determines if organ is suitable for harvest
                 if random.choice([True, False]):
