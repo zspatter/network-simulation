5f51b425ae3077ab2a0efd03523791fe074bbfb7
Zachary Patterson <localhost@zspatter.dev>
2019-04-16 16:54:45 -0400

    Adds test case for organ's path attribute

--- tests/test_organ.py
+++ tests/test_organ.py
@@ -8,18 +8,21 @@ def test_move_organ():
     assert test_organ.current_location is 1
     assert test_organ.origin_location is 1
     assert test_organ.viability is 60
+    assert test_organ.path == [test_organ.origin_location]
 
     # tests altered values are as expected
-    test_organ.move_organ(2, 20)
+    test_organ.move_organ(2, 20, ([1, 2, 3], 'weight'))
     assert test_organ.current_location is 2
     assert test_organ.origin_location is 1
     assert test_organ.viability is 40
+    assert test_organ.path == [1, 2, 3]
 
     # tests whether moving cost can be greater than current viability
-    test_organ.move_organ(3, 100)
+    test_organ.move_organ(3, 100, ([3, 2, 1], 'weight'))
     assert test_organ.current_location is 2
     assert test_organ.origin_location is 1
     assert test_organ.viability is 40
+    assert test_organ.path == [1, 2, 3]
 
 
 def test_organ_type_name():
