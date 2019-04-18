0d2937fb7a708c42868cfc709a79e4f8070f1efb
Zachary Patterson <zach.s.patterson@gmail.com>
2019-04-17 20:59:45 -0400

    Ensures organ_needed is assigned an int from the enum

--- network_simulator/PatientGenerator.py
+++ network_simulator/PatientGenerator.py
@@ -23,7 +23,7 @@ class PatientGenerator:
         for x in range(n):
             temp = P.Patient(patient_name="generated patient #" + str(x + 1),
                              illness="N/A",
-                             organ_needed=OrganType.get_organ_type(),
+                             organ_needed=OrganType.get_organ_type().value,
                              blood_type=bT.BloodType(BloodTypeLetter.get_blood_type(),
                                                      BloodTypePolarity.get_blood_polarity()),
                              priority=random.randrange(100 + n),
