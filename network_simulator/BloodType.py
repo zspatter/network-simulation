f8592fd0a92762131caea44f86efc51ec367fe87
Zachary Patterson <zach.s.patterson@gmail.com>
2019-04-19 12:31:50 -0400

    Fixes blood type compatability logic
    Previously, B could accept donations from A and vice versa. The logic has been adjusted to address this error.

--- network_simulator/BloodType.py
+++ network_simulator/BloodType.py
@@ -1,19 +1,30 @@
+from network_simulator.CompatibilityMarkers import BloodTypeLetter
+
+
 class BloodType:
     def __init__(self, blood_type_letter, blood_type_polarity):
         self.blood_type_letter = blood_type_letter
         self.blood_type_polarity = blood_type_polarity
 
     def is_compatible_recipient(self, blood_type):
-        if self.blood_type_letter.value >= blood_type.blood_type_letter.value \
-                and self.blood_type_polarity.value >= blood_type.blood_type_polarity.value:
-            return True
+        if self.blood_type_letter.value is not BloodTypeLetter.B.value:
+            return self.blood_type_letter.value >= blood_type.blood_type_letter.value \
+                   and self.blood_type_polarity.value >= blood_type.blood_type_polarity.value
+        else:
+            if blood_type.blood_type_letter.value in \
+                    (BloodTypeLetter.O.value, BloodTypeLetter.B.value):
+                return self.blood_type_polarity.value >= blood_type.blood_type_polarity.value
 
         return False
 
     def is_compatible_donor(self, blood_type):
-        if self.blood_type_letter.value <= blood_type.blood_type_letter.value \
-                and self.blood_type_polarity.value <= blood_type.blood_type_polarity.value:
-            return True
+        if self.blood_type_letter.value is not BloodTypeLetter.B.value:
+            return self.blood_type_letter.value <= blood_type.blood_type_letter.value \
+                   and self.blood_type_polarity.value <= blood_type.blood_type_polarity.value
+        else:
+            if blood_type.blood_type_letter.value in \
+                    (BloodTypeLetter.B.value, BloodTypeLetter.AB.value):
+                return self.blood_type_polarity <= blood_type.blood_type_polarity.value
 
         return False
 
