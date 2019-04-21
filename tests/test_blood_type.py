5631726719d5909130b4b46d906479989b8fc8e9
Zachary Patterson <zach.s.patterson@gmail.com>
2019-04-21 11:22:18 -0400

    Leverages collections of blood type pairs to test all possible combinations
    This makes the tests a bit easier to follow by removing the hard coded test cases. Furthermore, this is more thorough as it tests both donor and recipient compatability.

--- tests/test_blood_type.py
+++ tests/test_blood_type.py
@@ -1,85 +1,46 @@
 from network_simulator.BloodType import BloodType
 from network_simulator.CompatibilityMarkers import BloodTypeLetter, BloodTypePolarity
 
+o_neg = BloodType(BloodTypeLetter.O, BloodTypePolarity.NEG)
+o_pos = BloodType(BloodTypeLetter.O, BloodTypePolarity.POS)
+a_neg = BloodType(BloodTypeLetter.A, BloodTypePolarity.NEG)
+a_pos = BloodType(BloodTypeLetter.A, BloodTypePolarity.POS)
+b_neg = BloodType(BloodTypeLetter.B, BloodTypePolarity.NEG)
+b_pos = BloodType(BloodTypeLetter.B, BloodTypePolarity.POS)
+ab_neg = BloodType(BloodTypeLetter.AB, BloodTypePolarity.NEG)
+ab_pos = BloodType(BloodTypeLetter.AB, BloodTypePolarity.POS)
+
+valid_matches = [(o_neg, o_neg), (o_neg, o_pos), (o_neg, a_neg), (o_neg, a_pos),
+                 (o_neg, b_neg), (o_neg, b_pos), (o_neg, ab_neg), (o_neg, ab_pos),
+                 (o_pos, o_pos), (o_pos, a_pos), (o_pos, b_pos), (o_pos, ab_pos),
+                 (a_neg, a_neg), (a_neg, a_pos), (a_neg, ab_neg), (a_neg, ab_neg),
+                 (a_pos, a_pos), (a_pos, ab_pos), (b_neg, b_neg), (b_neg, b_pos),
+                 (b_neg, ab_neg), (b_neg, ab_pos), (b_pos, b_pos), (b_pos, ab_pos),
+                 (ab_neg, ab_neg), (ab_neg, ab_pos), (ab_pos, ab_pos)]
+
+invalid_matches = [(o_pos, o_neg), (o_pos, a_neg), (o_pos, b_neg), (o_pos, ab_neg),
+                   (a_neg, o_neg), (a_neg, o_pos), (a_neg, b_neg), (a_neg, b_pos),
+                   (a_pos, o_neg), (a_pos, o_pos), (a_pos, a_neg), (a_pos, b_neg),
+                   (a_pos, b_pos), (a_pos, ab_neg), (b_neg, o_neg), (b_neg, o_pos),
+                   (b_neg, a_neg), (b_neg, a_pos), (b_pos, o_neg), (b_pos, o_pos),
+                   (b_pos, a_neg), (b_pos, a_pos), (b_pos, b_neg), (b_pos, ab_neg),
+                   (ab_neg, o_neg), (ab_neg, o_pos), (ab_neg, a_neg), (ab_neg, a_pos),
+                   (ab_neg, b_neg), (ab_neg, b_pos), (ab_pos, o_neg), (ab_pos, o_pos),
+                   (ab_pos, a_neg), (ab_pos, a_pos), (ab_pos, b_neg), (ab_pos, b_pos),
+                   (ab_pos, ab_neg)]
+
 
 def test_is_compatible_donor():
-    o_neg = BloodType(BloodTypeLetter.O, BloodTypePolarity.NEG)
-    o_pos = BloodType(BloodTypeLetter.O, BloodTypePolarity.POS)
-    a_neg = BloodType(BloodTypeLetter.A, BloodTypePolarity.NEG)
-    a_pos = BloodType(BloodTypeLetter.A, BloodTypePolarity.POS)
-    b_neg = BloodType(BloodTypeLetter.B, BloodTypePolarity.NEG)
-    b_pos = BloodType(BloodTypeLetter.B, BloodTypePolarity.POS)
-    ab_neg = BloodType(BloodTypeLetter.AB, BloodTypePolarity.NEG)
-    ab_pos = BloodType(BloodTypeLetter.AB, BloodTypePolarity.POS)
+    for donor, recipient in valid_matches:
+        assert donor.is_compatible_donor(recipient)
 
-    assert o_neg.is_compatible_donor(o_neg)
-    assert o_neg.is_compatible_donor(o_pos)
-    assert o_neg.is_compatible_donor(a_neg)
-    assert o_neg.is_compatible_donor(a_pos)
-    assert o_neg.is_compatible_donor(b_neg)
-    assert o_neg.is_compatible_donor(b_pos)
-    assert o_neg.is_compatible_donor(ab_neg)
-    assert o_neg.is_compatible_donor(ab_pos)
+    for donor, recipient in invalid_matches:
+        assert not donor.is_compatible_donor(recipient)
 
-    assert not o_pos.is_compatible_donor(o_neg)
-    assert o_pos.is_compatible_donor(o_pos)
-    assert not o_pos.is_compatible_donor(a_neg)
-    assert o_pos.is_compatible_donor(a_pos)
-    assert not o_pos.is_compatible_donor(b_neg)
-    assert o_pos.is_compatible_donor(b_pos)
-    assert not o_pos.is_compatible_donor(ab_neg)
-    assert o_pos.is_compatible_donor(ab_pos)
 
-    assert not a_neg.is_compatible_donor(o_neg)
-    assert not a_neg.is_compatible_donor(o_pos)
-    assert a_neg.is_compatible_donor(a_neg)
-    assert a_neg.is_compatible_donor(a_pos)
-    assert not a_neg.is_compatible_donor(b_neg)
-    assert not a_neg.is_compatible_donor(b_pos)
-    assert a_neg.is_compatible_donor(ab_neg)
-    assert a_neg.is_compatible_donor(ab_pos)
+def test_is_compatible_recipient():
+    for donor, recipient in valid_matches:
+        assert recipient.is_compatible_recipient(donor)
 
-    assert not a_pos.is_compatible_donor(o_neg)
-    assert not a_pos.is_compatible_donor(o_pos)
-    assert not a_pos.is_compatible_donor(a_neg)
-    assert a_pos.is_compatible_donor(a_pos)
-    assert not a_pos.is_compatible_donor(b_neg)
-    assert not a_pos.is_compatible_donor(b_pos)
-    assert not a_pos.is_compatible_donor(ab_neg)
-    assert a_pos.is_compatible_donor(ab_pos)
-
-    assert not b_neg.is_compatible_donor(o_neg)
-    assert not b_neg.is_compatible_donor(o_pos)
-    assert not b_neg.is_compatible_donor(a_neg)
-    assert not b_neg.is_compatible_donor(a_pos)
-    assert b_neg.is_compatible_donor(b_neg)
-    assert b_neg.is_compatible_donor(b_pos)
-    assert b_neg.is_compatible_donor(ab_neg)
-    assert b_neg.is_compatible_donor(ab_pos)
-
-    assert not b_pos.is_compatible_donor(o_neg)
-    assert not b_pos.is_compatible_donor(o_pos)
-    assert not b_pos.is_compatible_donor(a_neg)
-    assert not b_pos.is_compatible_donor(a_pos)
-    assert not b_pos.is_compatible_donor(b_neg)
-    assert b_pos.is_compatible_donor(b_pos)
-    assert not b_pos.is_compatible_donor(ab_neg)
-    assert b_pos.is_compatible_donor(ab_pos)
-
-    assert not ab_neg.is_compatible_donor(o_neg)
-    assert not ab_neg.is_compatible_donor(o_pos)
-    assert not ab_neg.is_compatible_donor(a_neg)
-    assert not ab_neg.is_compatible_donor(a_pos)
-    assert not ab_neg.is_compatible_donor(b_neg)
-    assert not ab_neg.is_compatible_donor(b_pos)
-    assert ab_neg.is_compatible_donor(ab_neg)
-    assert ab_neg.is_compatible_donor(ab_pos)
-
-    assert not ab_pos.is_compatible_donor(o_neg)
-    assert not ab_pos.is_compatible_donor(o_pos)
-    assert not ab_pos.is_compatible_donor(a_neg)
-    assert not ab_pos.is_compatible_donor(a_pos)
-    assert not ab_pos.is_compatible_donor(b_neg)
-    assert not ab_pos.is_compatible_donor(b_pos)
-    assert not ab_pos.is_compatible_donor(ab_neg)
-    assert ab_pos.is_compatible_donor(ab_pos)
+    for donor, recipient in invalid_matches:
+        assert not recipient.is_compatible_recipient(donor)
