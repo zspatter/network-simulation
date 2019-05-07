e491584d30babf94199d3f8870aad80c87a8f695
zspatter <localhost@zspatter.dev>
2019-05-07 15:41:13 -0400

    Removes second demo case

--- network_simulator/execute/subnetwork_demo.py
+++ network_simulator/execute/subnetwork_demo.py
@@ -3,7 +3,7 @@ from network_simulator.PatientGenerator import PatientGenerator
 from network_simulator.OrganGenerator import OrganGenerator
 from network_simulator.WaitList import WaitList
 from network_simulator.OrganList import OrganList
-from network_simulator.GenerateSubnetwork import GenerateSubnetwork
+from network_simulator.SubnetworkGenerator import SubnetworkGenerator
 
 
 network = GraphBuilder.graph_builder(20)
@@ -21,8 +21,25 @@ print(organ_list)
 # print(isinstance(wait_list, WaitList), isinstance(organ_list, OrganList))
 # print(type(wait_list), type(organ_list))
 
-patient_network = GenerateSubnetwork.generate_subnetwork(network, wait_list)
+patient_network = SubnetworkGenerator.generate_subnetwork(network, wait_list)
 print(patient_network)
 
-organ_network = GenerateSubnetwork.generate_subnetwork(network, organ_list)
+organ_network = SubnetworkGenerator.generate_subnetwork(network, organ_list)
+print(organ_network)
+
+# creates network
+network = GraphBuilder.graph_builder(10)
+# graph_nx = GraphConverter.convert_to_networkx(network)
+
+# creates patient list
+wait_list = WaitList()
+patients = PatientGenerator.generate_patients(network, 5, wait_list)
+# creates organ list
+organ_list = OrganList()
+organs = OrganGenerator.generate_organs(network, 5, organ_list)
+print(network)
+
+patient_network = SubnetworkGenerator.generate_subnetwork(network, wait_list)
+organ_network = SubnetworkGenerator.generate_subnetwork(network, organ_list)
+print(patient_network)
 print(organ_network)
