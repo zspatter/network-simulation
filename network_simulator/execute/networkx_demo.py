944214891473bb65338bf3523c01b329be72c576
zspatter <localhost@zspatter.dev>
2019-05-07 15:43:57 -0400

    Adds sample demo that converts and plots a hospital network, then repeats this with patient/organ subnetworks

--- network_simulator/execute/networkx_demo.py
+++ network_simulator/execute/networkx_demo.py
@@ -3,65 +3,81 @@ import matplotlib.pyplot as plt
 from network_simulator.Node import Node
 from network_simulator.Network import Network
 from network_simulator.GraphConverter import GraphConverter
+from network_simulator.GraphBuilder import GraphBuilder
+from network_simulator.WaitList import WaitList
+from network_simulator.PatientGenerator import PatientGenerator
+from network_simulator.OrganList import OrganList
+from network_simulator.OrganGenerator import OrganGenerator
+from network_simulator.SubnetworkGenerator import SubnetworkGenerator
 
 
-node1 = Node(1, "A",
-             {2: {'weight': 3, 'status': True},
-              3: {'weight': 5, 'status': True},
-              6: {'weight': 6, 'status': False}})
-node2 = Node(2, "B",
-             {1: {'weight': 20, 'status': True},
-              4: {'weight': 3, 'status': True}})
-node3 = Node(3, "C",
-             {2: {'weight': 6, 'status': True},
-              4: {'weight': 9, 'status': True}})
-node4 = Node(4, "D",
-             {3: {'weight': 8, 'status': True},
-              2: {'weight': 4, 'status': True}})
-node5 = Node(5, "E",
-             {1: {'weight': 1, 'status': True},
-              4: {'weight': 2, 'status': True}})
-
-net = Network({node1.node_id: node1,
-               node2.node_id: node2,
-               node3.node_id: node3,
-               node4.node_id: node4,
-               node5.node_id: node5})
-
-G = GraphConverter.convert_to_networkx(net)
-
-# G = nx.Graph()
-# G.add_node(1)
-# G.add_nodes_from([2, 3])
-# G.add_edge(1, 2)
+# node1 = Node(1, "A",
+#              {2: {'weight': 3, 'status': True},
+#               3: {'weight': 5, 'status': True},
+#               6: {'weight': 6, 'status': False}})
+# node2 = Node(2, "B",
+#              {1: {'weight': 20, 'status': True},
+#               4: {'weight': 3, 'status': True}})
+# node3 = Node(3, "C",
+#              {2: {'weight': 6, 'status': True},
+#               4: {'weight': 9, 'status': True}})
+# node4 = Node(4, "D",
+#              {3: {'weight': 8, 'status': True},
+#               2: {'weight': 4, 'status': True}})
+# node5 = Node(5, "E",
+#              {1: {'weight': 1, 'status': True},
+#               4: {'weight': 2, 'status': True}})
 #
-# print(list(nx.connected_components(G)))
-#
-# d = {1: {2: {'weight': 10}},
-#      2: {3: {'weight': 9}, 4: {'weight': 3}, 6: {'weight': 4}},
-#      3: {4: {'weight': 8}},
-#      4: {5: {'weight': 7}},
-#      5: {6: {'weight': 6}},
-#      6: {7: {'weight': 5}}}
-#
-# G = nx.Graph()
-# G.add_edge(1, 2, weight=10)
-# G.add_edge(2, 3, weight=9)
-# G.add_edge(3, 4, weight=8)
-# G.add_edge(4, 5, weight=7)
-# G.add_edge(5, 3, weight=30)
-# G.add_edge(2, 1, weight=10)
+# net = Network({node1.node_id: node1,
+#                node2.node_id: node2,
+#                node3.node_id: node3,
+#                node4.node_id: node4,
+#                node5.node_id: node5})
 
-pos = nx.spring_layout(G)
-# nx.draw(G, pos=pos, with_labels=True)
-# nx.draw_networkx_edge_labels(G, pos)
-edge_weights = {(u, v,): d['weight'] for u, v, d in G.edges(data=True)}
 
-# print(edge_weights)
-# for u, v, d in G.edges(data=True):
-#     print(u, v, d)
+# creates network
+network = GraphBuilder.graph_builder(20)
 
-nx.draw(G, pos=pos, with_labels=True)
-nx.draw_networkx_edge_labels(G, pos, edge_weights)
+# creates patient list
+wait_list = WaitList()
+PatientGenerator.generate_patients(network, 10, wait_list)
+print(wait_list)
 
+# creates organ list
+organ_list = OrganList()
+OrganGenerator.generate_organs(network, 10, organ_list)
+print(organ_list)
+
+
+# creates subnetworks for patients and organs
+patient_network = SubnetworkGenerator.generate_subnetwork(network, wait_list)
+organ_network = SubnetworkGenerator.generate_subnetwork(network, organ_list)
+
+# generates NetworkX graphs
+graph_nx = GraphConverter.convert_to_networkx(network)
+patient_nx = GraphConverter.convert_to_networkx(patient_network)
+organ_nx = GraphConverter.convert_to_networkx(organ_network)
+
+
+# plots hospital network (overall/global network)
+pos = nx.spring_layout(graph_nx)
+edge_weights = {(u, v,): d['weight'] for u, v, d in graph_nx.edges(data=True)}
+nx.draw(graph_nx, pos=pos, with_labels=True)
+nx.draw_networkx_edge_labels(graph_nx, pos, edge_weights)
+plt.show()
+
+
+# plots patient network
+pos = nx.spring_layout(patient_nx)
+edge_weights = {(u, v,): d['weight'] for u, v, d in patient_nx.edges(data=True)}
+nx.draw(patient_nx, pos=pos, with_labels=True)
+nx.draw_networkx_edge_labels(patient_nx, pos, edge_weights)
+plt.show()
+
+
+# plots organ network
+pos = nx.spring_layout(organ_nx)
+edge_weights = {(u, v,): d['weight'] for u, v, d in organ_nx.edges(data=True)}
+nx.draw(organ_nx, pos=pos, with_labels=True)
+nx.draw_networkx_edge_labels(organ_nx, pos, edge_weights)
 plt.show()
--- network_simulator/execute/subnetwork_demo.py
+++ network_simulator/execute/subnetwork_demo.py
@@ -26,20 +26,3 @@ print(patient_network)
 
 organ_network = SubnetworkGenerator.generate_subnetwork(network, organ_list)
 print(organ_network)
-
-# creates network
-network = GraphBuilder.graph_builder(10)
-# graph_nx = GraphConverter.convert_to_networkx(network)
-
-# creates patient list
-wait_list = WaitList()
-patients = PatientGenerator.generate_patients(network, 5, wait_list)
-# creates organ list
-organ_list = OrganList()
-organs = OrganGenerator.generate_organs(network, 5, organ_list)
-print(network)
-
-patient_network = SubnetworkGenerator.generate_subnetwork(network, wait_list)
-organ_network = SubnetworkGenerator.generate_subnetwork(network, organ_list)
-print(patient_network)
-print(organ_network)
