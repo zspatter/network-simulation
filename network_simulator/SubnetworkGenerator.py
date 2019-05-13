918692e83ddaeba191c15a3939e329bac52c226a
zspatter <localhost@zspatter.dev>
2019-05-13 13:22:03 -0400

    Adds docstring function definitions

--- network_simulator/SubnetworkGenerator.py
+++ network_simulator/SubnetworkGenerator.py
@@ -9,6 +9,15 @@ from network_simulator.WaitList import WaitList
 class SubnetworkGenerator:
     @staticmethod
     def generate_subnetwork(network: Network, collection: Union[OrganList, WaitList]) -> Network:
+        """
+        Takes a graph and a collection and creates a subnetwork that
+        only consists of nodes which are present in the collection.
+        (generates a subnetwork of patients OR organs)
+        
+        :param Network network: global network of hospitals
+        :param collection: list of patients OR organs
+        :return:
+        """
         subnetwork = SubnetworkGenerator.copy_network(network)
         SubnetworkGenerator.mark_network_inactive(subnetwork)
         active_nodes: Set[int] = set()
@@ -31,9 +40,22 @@ class SubnetworkGenerator:
     
     @staticmethod
     def copy_network(network: Network) -> Network:
+        """
+        Takes a Network object and returns a deep copy of the network
+        (this is what the subnetwork is built upon)
+        
+        :param Network network: network to be copied (global network)
+        :return: Network copy
+        """
         return copy.deepcopy(network)
     
     @staticmethod
     def mark_network_inactive(network: Network) -> None:
+        """
+        Marks all nodes in a given network inactive
+        (this is the starting point for the subnetwork)
+        
+        :param Network network: graph that is the foundation for subnetworks
+        """
         for node in network.nodes():
             network.mark_node_inactive(node, feedback=False)
