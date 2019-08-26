c4e7cf59f334f12dccf57635bbec4a7fc78b59a4
zspatter <localhost@zspatter.dev>
2019-08-26 09:08:42 -0400

    modifies scripts to add attributes to the graphs
    this is used to quanitfy the number of hospitals in a given area

--- execute/export_hospital_network.py
+++ execute/export_hospital_network.py
@@ -1,5 +1,6 @@
 import shelve
 from os.path import abspath, basename, join
+from pathlib import Path
 
 import networkx
 
@@ -9,6 +10,8 @@ from network_simulator.GraphConverter import GraphConverter
 def export_gexf(import_path='.',
                 shelve_key='hospital_network2',
                 export_path='hospital_network.gexf',
+                state_dict=None,
+                region_dict=None,
                 is_regional_weight=None):
     """
     This function exports the shelve_variable value within the shelve file
@@ -18,14 +21,23 @@ def export_gexf(import_path='.',
     :param str import_path: shelve filepath
     :param str shelve_key: name of key network is stored as
     :param str export_path: export filepath
+    :param dict state_dict: quanitfies how many hospitals share the state
+    :param dict region_dict: quanitfies how many hospitals share the region
     :param bool is_regional_weight: indicates whether or not regional
             weight should be weight metric
     """
     db = shelve.open(filename=import_path)
     hospital_network = db[shelve_key]
 
-    nx = GraphConverter.convert_to_networkx(network=hospital_network,
-                                            is_regional_weight=is_regional_weight)
+    if state_dict or region_dict:
+        nx = GraphConverter.convert_to_attribute_nx(network=hospital_network,
+                                                    is_regional_weight=is_regional_weight,
+                                                    state_dict=state_dict,
+                                                    region_dict=region_dict)
+    else:
+        nx = GraphConverter.convert_to_networkx(network=hospital_network,
+                                                is_regional_weight=is_regional_weight)
+
     networkx.write_gexf(G=nx, path=export_path)
     print(f"'{basename(export_path)}' has been successfully exported to: {export_path}\n")
 
@@ -33,9 +45,18 @@ def export_gexf(import_path='.',
 if __name__ == '__main__':
     shelve_path = join(abspath('.'), 'export', 'shelve', 'distance_vector')
     export_root = join(abspath('.'), 'export', 'gexf')
+    area_counts = shelve.open(str(Path('./export/shelve/hospital_quantities')))
+    states = area_counts['states']
+    regions = area_counts['regions']
 
+    # export_gexf(import_path=shelve_path,
+    #             export_path=join(export_root, 'hospital_network_distance2.gexf'))
+    # export_gexf(import_path=shelve_path,
+    #             export_path=join(export_root, 'hospital_network_regional2.gexf'),
+    #             is_regional_weight=True)
     export_gexf(import_path=shelve_path,
-                export_path=join(export_root, 'hospital_network_distance2.gexf'))
-    export_gexf(import_path=shelve_path,
-                export_path=join(export_root, 'hospital_network_regional2.gexf'),
-                is_regional_weight=True)
+                export_path=join(export_root, 'hospital_network_attributes.gexf'),
+                state_dict=states,
+                region_dict=regions)
+
+
--- network_simulator/GraphConverter.py
+++ network_simulator/GraphConverter.py
@@ -21,12 +21,47 @@ class GraphConverter:
 
         for node in nodes:
             nx_graph.add_node(node)
-            adjacents = network.network_dict[node].get_adjacents()
-            for adjacent in adjacents:
-                if network.network_dict[node].adjacency_dict[adjacent]['status']:
-                    weight = network.network_dict[node].adjacency_dict[adjacent]['weight'] \
-                        if not is_regional_weight else \
-                        network.network_dict[node].adjacency_dict[adjacent]['regional weight']
-                    nx_graph.add_edge(node, adjacent, weight=weight)
+            GraphConverter.add_edges(network, node, nx_graph, is_regional_weight)
 
         return nx_graph
+
+    @staticmethod
+    def convert_to_attribute_nx(network: Network,
+                                is_regional_weight: bool = None,
+                                state_dict=None,
+                                region_dict=None) -> nx.Graph:
+        """
+        Converts a Network object to a NetworkX graph
+
+        :param Network network:
+        :param bool is_regional_weight: indicates whether weight metric should
+                be regional weight or distance
+        :param dict state_dict: quanitfies how many hospitals share the state
+        :param dict region_dict: quanitfies how many hospitals share the region
+        :return: NetworkX Graph
+        """
+        nx_graph = nx.Graph()
+        nodes = network.nodes()
+
+        for node in nodes:
+            state = network.network_dict[node].state
+            state = 'Washington D.C.' if state == 'US' else state
+            region = network.network_dict[node].region
+
+            nx_graph.add_node(node,
+                              state_count=state_dict[state],
+                              region_count=region_dict[region])
+
+            GraphConverter.add_edges(network, node, nx_graph, is_regional_weight)
+
+        return nx_graph
+
+    @staticmethod
+    def add_edges(network, node, nx_graph, is_regional_weight):
+        adjacents = network.network_dict[node].get_adjacents()
+        for adjacent in adjacents:
+            if network.network_dict[node].adjacency_dict[adjacent]['status']:
+                weight = network.network_dict[node].adjacency_dict[adjacent]['weight'] \
+                    if not is_regional_weight else \
+                    network.network_dict[node].adjacency_dict[adjacent]['regional weight']
+                nx_graph.add_edge(node, adjacent, weight=weight)
