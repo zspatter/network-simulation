b7740bc8a4caef349a094939ce7a85fc02dbe64f
zspatter <localhost@zspatter.dev>
2019-08-28 12:31:38 -0400

    Adds missing docstring function definitions

--- execute/export_edgelist.py
+++ execute/export_edgelist.py
@@ -2,7 +2,7 @@ import shelve
 from pathlib import Path
 
 
-def generate_edgelist(network, output_path=Path('edgelist.txt')):
+def generate_edge_list(network, output_path=Path('edgelist.txt')):
     """
     Takes a network object and exports it's edge list to the output path
 
@@ -17,6 +17,13 @@ def generate_edgelist(network, output_path=Path('edgelist.txt')):
 
 
 def unique_edge_list(network):
+    """
+    Generates an edge list with only unique edges. This removes duplicates
+    resulting from the bidirectional nature of the edges.
+
+    :param network_simulator.Network network: source network
+    :return: list edge_list: unique set of edges
+    """
     edge_list = []
     nodes = [network.network_dict[node] for node in network.nodes()]
 
@@ -32,6 +39,13 @@ def unique_edge_list(network):
 
 
 def write_edge_list(edge_list, output_path):
+    """
+    Takes an edge list (list of integer pairs as tuples) and
+    exports the edge list as plaintext
+
+    :param list edge_list: list of unique edges (node pairs)
+    :param Path output_path: path to resulting output
+    """
     with output_path.open('w') as out:
         lines = '\n'.join(map(lambda x: f'{x[0]} {x[1]}', edge_list))
         out.writelines(lines)
@@ -40,8 +54,8 @@ def write_edge_list(edge_list, output_path):
 if __name__ == '__main__':
     db = shelve.open(str(Path('./export/shelve/distance_vector')))
     hospital_network = db['hospital_network2']
-    # generate_edgelist(network=hospital_network,
-    #                   output_path=Path('./export/edgelist/hospital_edge_list2'))
+    # generate_edge_list(network=hospital_network,
+    #                    output_path=Path('./export/edgelist/hospital_edge_list2'))
 
     edges = unique_edge_list(network=hospital_network)
     write_edge_list(edge_list=edges,
